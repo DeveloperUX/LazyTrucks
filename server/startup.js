fb98ecc0dcf2031ebd46e75fc696d898276d553d
Moustafa Ismael <mismael7@gmail.com>
2015-12-02 23:53:27 -0500

    Able to retrieve and combine food trucks from both sources and display them

---
+++ server/startup.js
@@ -0,0 +1,53 @@
+/**
+ * Code to be called on the Server when the app starts up
+ */
+Meteor.startup(function() {
+
+  // TODO : Development stuff that'll need to be removed later
+
+  // First wipe out the list of trucks
+  Trucks.remove({});
+  Menus.remove({});
+
+  // TODO : Need to run this as a cron job every hour
+
+  var trucks = [];
+
+  // Grab the complete list of food trucks from those guys
+  var foodTrucksList = HTTP.call("GET", "http://foodtruckfiesta.com/apps/map_json.php?num_days=365&minimal=0&alert_nc=y&alert_hc=0&alert_pm=0&rand=458950514");
+  console.log(foodTrucksList.data.markers);
+  _.forEach(foodTrucksList.data.markers, function(marker) {
+    // Let's grab only the fields we need
+    let truck = _.pick(marker, 'coord_lat', 'coord_long', 'truck', 'print_name');
+    // Rename some fields, handle will be the document ID
+    truck.handle = truck.truck.toUpperCase();
+    truck.name = truck.print_name;
+    truck = _.omit(truck, ['truck', 'print_name']);
+    // Add the truck to our full list of trucks
+    trucks.push(truck);
+  });
+
+  // Add the list of trucks from the seed file
+  var trucksFromSeed = JSON.parse(Assets.getText('seed.json'));
+  _.forEach(trucksFromSeed, function(truckFromSeed) {
+    // If the Truck already exists, simply append the information
+    var existing = _.findWhere(trucks, {handle: truckFromSeed.handle.toUpperCase()});
+    if( existing ) {
+      // Add the fields from the seed file objects (url and image)
+      existing.url = truckFromSeed.url;
+      existing.image = truckFromSeed.image;
+
+      console.log("Found an existing truck: ", existing);
+    }
+    // If this truck doesn't exist, add it to the global list of trucks
+    else {
+      trucks.push(truckFromSeed);
+    }
+
+  });
+
+  trucks.forEach( function(truck) {
+    Trucks.insert(truck);
+  });
+
+});
