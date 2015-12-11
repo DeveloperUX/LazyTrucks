9ecfaab7a0487584be561cd1050473b624f14bc3
Smiley <ismael_moustafa@bah.com>
2015-12-10 19:09:25 -0500

    Fixed cards so they are equal height and Food Truck name overlaps the Truck image

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,13 +2,16 @@
 Card = React.createClass({
   render() {
     return (
-      <div className="card">
+      <div className="card medium">
         <div className="card-image">
           <img src={this.props.image} />
+          <span className="card-title">{this.props.link}</span>
         </div>
+        {/*
         <div className="card-action">
           <a href={this.props.link}>{this.props.linkText}</a>
         </div>
+        */}
       </div>
     );
   }
